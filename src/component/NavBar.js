3d87b92c3716e8e0ef78e368c14f0df4bee8b578
skhalamai40611 <sofiia.khalamai@cengage.com>
2023-10-04 19:04:49 +0300

    add changes

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from '../img/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FaBars, FaTimes} from "react-icons/fa"
-import { useRef } from "react";
-import "../css/NavBar.css";
+import { FaBars, FaTimes } from 'react-icons/fa';
+import '../css/NavBar.css';
 
 function Navbar() {
-	const navRef = useRef();
+  const navRef = useRef();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+  const showNavbar = () => {
+    setIsNavOpen(!isNavOpen);
+  };
 
-	return (
-		<header>
-			<a href="/">
-      	<img src={logo} class="d-inline-block align-middle mr-4"/>
-			</a>
-			<nav ref={navRef}>
-				<a href="#courses">Курси</a>
-				<a href="#interactive">Інтерактив</a>
-				<a href="#feedbacks">Викладачі та відгуки</a>
-				<a href="#rules">Правила</a>
+  const closeNavbar = () => {
+    setIsNavOpen(false);
+  };
+
+  return (
+    <header>
+      <a href="/">
+        <img src={logo} className="d-inline-block align-middle mr-4" />
+      </a>
+      <nav
+        ref={navRef}
+        className={isNavOpen ? 'responsive_nav' : ''}
+        onClick={closeNavbar}
+      >
+        <a href="#courses">Курси</a>
+        <a href="#interactive">Інтерактив</a>
+        <a href="#feedbacks">Викладачі та відгуки</a>
+        <a href="#rules">Правила</a>
         <a href="#contacts">Контакти</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+        <button className="nav-btn nav-close-btn">
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
